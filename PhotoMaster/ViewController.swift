8dd9c23e74f81ec792c38e2dc3e7e3be2a9b396b
okada hatsune <swan.nilda1211@gmail.com>
2020-05-11 14:27:48 +0900

    Initial Commit

---
+++ PhotoMaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoMaster
+//
+//  Created by Hatsune Okada on 5/11/20.
+//  Copyright © 2020 Okada Hattsunn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
